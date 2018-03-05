be00a4b3c3c3222347bd2d662bdeac42571e34c9
Jonathan Delgado Zamorano <hi@jon.soy>
2018-03-05 16:53:04 -0300

    Added eslint and fix code style with airbnb style

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+/* eslint global-require:off */
 
 module.exports = {
-    MongooseFacade: require('./lib/mongoose-facade'),
-    Controller: require('./lib/controller'),
-    Errors: require('./lib/errors'),
-    Logger: require('./lib/logger')
-}
+  MongooseFacade: require('./lib/mongoose-facade'),
+  Controller: require('./lib/controller'),
+  Errors: require('./lib/errors'),
+  Logger: require('./lib/logger'),
+};
